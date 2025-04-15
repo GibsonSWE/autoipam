045346b70d6c17e4ccf6965e6612866b7bce47c8
Jacob Henriksen <jacob.henriksen94@gmail.com>
2025-04-15 02:56:34 +0200

    Created config.ini and made required changes to constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,34 +1,53 @@
 import os
 import ipaddress
+from configparser import ConfigParser
 
+# This file contains all the constants used in the project.
+
+
+# Opens the config.ini file and reads the configuration
+try:
+    config_file = os.path.join(os.path.dirname(__file__), '../config.ini')
+    config = ConfigParser()
+    config.read(config_file)
+except FileNotFoundError:
+    print(f"Error: Configuration file not found at {config_file}. Please check the path.")
+    raise
+except Exception as e:
+    print(f"Error reading configuration file: {e}")
+    raise
+
+
+# Current version and release date
 RELEASE = {'version': 'v0.2.0 (Beta)', 'date': '2025-04-14'}
 
-SECTION_ID = 3 #SCA
+# Section ID used for phpIpam API calls
+SECTION_ID = config.get('phpIpam', 'section_id')
+
+# Paths where the reports are saved
 SUBNET_REPORT_PATH = '/var/autoipam-reports/subnet-reports/'   
 ADDRESS_REPORT_PATH = '/var/autoipam-reports/address-reports/'
 CONFLICTS_PATH = '/var/autoipam-reports/conflicts/'             
 DIFF_PATH = '/var/autoipam-reports/diff/'
 
 
+# Report file names
 # Time stamp and unique identifier are set in functions in utils.py
 SUBNET_REPORT_FILE_NAME = 'autoipam_report_subnets'
 ADDRESS_REPORT_FILE_NAME = 'autoipam_report_addresses'
 DIFF_EXPORT_FILE_NAME = 'autoipam_diff'
 CONFLICT_FILE_NAME = 'update_conflicts'
 
+# API keys for the different services
+IPAM_API_KEY = config.get('phpIpam', 'api_key')
+CHECKPOINT_API_KEY = config.get('Checkpoint', 'api_key')
+DNAC_API_KEY = config.get('DNA-Center', 'api_key')
 
-IPAM_API_KEY = os.environ.get('AUTOIPAM_IPAM_API_KEY')
-CHECKPOINT_API_KEY = os.environ.get('AUTOIPAM_CHECKPOINT_API_KEY')
-DNAC_API_KEY = os.environ.get('AUTOIPAM_DNAC_API_KEY')
-DNAC_USERNAME = 'autoipam'
+DNAC_USERNAME = config.get('DNA-Center', 'username')
+IPAM_APP_ID = config.get('phpIpam', 'app_id')
 
-
-IGNORED_IP_RANGES = [
-    '0.0.0.0/32', 
-    '10.200.252.33/30',     # Checkpoint Sync Networks
-    '192.168.0.0/16',       # NAT Networks?
-    '172.16.0.0/16'         # NAT Networks?
-]
+# IP ranges that the script should ignore
+IGNORED_IP_RANGES = config.get('IP Configs', 'ignored_ip_ranges')
 
 
 # VRFs
@@ -77,20 +96,18 @@ SCA_DMZ_VRF = [
 
 
 # IPAM endpoints
-IPAM_URL = 'https://ipam.sca.com'
-#IPAM_URL = 'https://ipamtest.sca.com'  #TEST URL
-APP_ID = 'AutoIpam'
+IPAM_URL = config.get('phpIpam', 'url')
 
-IPAM_GET_CUSTOM_FIELDS = f'/api/{APP_ID}/addresses/custom_fields/'
-IPAM_ADDRESSES = f'/api/{APP_ID}/addresses/'
-IPAM_GET_SUBNET = f'/api/{APP_ID}/subnets/cidr/'#{subnet}/ in CIDR format
-IPAM_GET_VRFS = f'/api/{APP_ID}/vrf/'
-IPAM_CREATE_SUBNET = f'/api/{APP_ID}/subnets/'
-IPAM_SEARCH_ADDRESS = f'/api/{APP_ID}/addresses/search/'#{ip}/
+IPAM_GET_CUSTOM_FIELDS = f'/api/{IPAM_APP_ID}/addresses/custom_fields/'
+IPAM_ADDRESSES = f'/api/{IPAM_APP_ID}/addresses/'
+IPAM_GET_SUBNET = f'/api/{IPAM_APP_ID}/subnets/cidr/'#{subnet}/ in CIDR format
+IPAM_GET_VRFS = f'/api/{IPAM_APP_ID}/vrf/'
+IPAM_CREATE_SUBNET = f'/api/{IPAM_APP_ID}/subnets/'
+IPAM_SEARCH_ADDRESS = f'/api/{IPAM_APP_ID}/addresses/search/'#{ip}/
 
 
 # Checkpoint endpoints
-CHECKPOINT_URL = 'https://S1PRMGM0004.forestproducts.sca.com'
+CHECKPOINT_URL = config.get('Checkpoint', 'url')
 
 CHECKPOINT_AUTH = '/web_api/login'
 CHECKPOINT_SHOW_NETWORKS = '/web_api/show-networks'
@@ -101,7 +118,7 @@ CHECKPOINT_SHOW_OBJECT = '/web_api/show-object'
 
 
 # DNA-center endpoints
-DNAC_URL = 'https://dnac.forestproducts.sca.com'
+DNAC_URL = config.get('DNA-Center', 'url')
 
 DNAC_AUTH = '/dna/system/api/v1/auth/token/'
 DNAC_NETWORK_DEVICE = '/dna/intent/api/v1/network-device/'
@@ -109,5 +126,5 @@ DNAC_INTERFACES = '/dna/intent/api/v1/interface/network-device/'#{deviceId}
 
 
 # vManage endpoints
-VMANAGE_URL = ''
+VMANAGE_URL = config.get('Vmanage', 'url')
 VMANAGE_AUTH = ''
