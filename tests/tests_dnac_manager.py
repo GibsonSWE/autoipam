318ec71ba9c372ce907b56b399f1bef8816a78b7
Jacob Henriksen <jacob.henriksen94@gmail.com>
2025-04-16 02:28:19 +0200

    Added tests directory and created test modules for each module

---
+++ tests/tests_dnac_manager.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+import os
+import unittest.mock
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.cli import CLI
+
+class TestDNACManager(unittest.TestCase):
+    pass
+
